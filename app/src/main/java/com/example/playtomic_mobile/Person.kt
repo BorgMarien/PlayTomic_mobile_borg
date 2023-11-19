389e1e077a172cbb463a2464b23d08b94a3a2f30
BorgMarien <borgmarien@gmail.com>
2023-11-19 21:14:56 +0100

    get basic firebase data on profile

---
+++ app/src/main/java/com/example/playtomic_mobile/Person.kt
@@ -0,0 +1,10 @@
+package com.example.playtomic_mobile
+
+data class Person (
+    var Firstname : String ,
+    var LastName : String,
+    var HomeLocation: String,
+    var MatchType: String,
+    var PreferedPlayTime:String,
+    var CourtPosition: String
+)
--- app/src/main/java/com/example/playtomic_mobile/Profile.kt
+++ app/src/main/java/com/example/playtomic_mobile/Profile.kt
@@ -7,13 +7,27 @@ import android.os.Bundle
 import android.util.Log
 import android.view.View
 import android.widget.TextView
+import com.beust.klaxon.JsonArray
+import com.beust.klaxon.JsonObject
+import com.beust.klaxon.Parser
 import com.google.firebase.Firebase
 import com.google.firebase.firestore.firestore
+import java.lang.StringBuilder
 
 class Profile : Activity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.profile_layout)
+        //persons
+        val ProfileData:Person ;
+
+        //values
+        val name = findViewById<View>(R.id.name) as TextView
+        val location = findViewById<View>(R.id.location) as TextView
+        val hand = findViewById<View>(R.id.hand) as TextView
+        val courtPosition = findViewById<View>(R.id.court) as TextView
+        val matchType = findViewById<View>(R.id.match) as TextView
+        val PreferedPlayTime = findViewById<View>(R.id.playtime) as TextView
 
         //firebase
         val db = Firebase.firestore
@@ -23,12 +37,18 @@ class Profile : Activity() {
             .get()
             .addOnSuccessListener { result ->
                 for (document in result) {
-                    Log.d(TAG, "${document.id} => ${document.data}")
+                    //TODO change this so it doenst just take the first one
+
+                    Log.d(TAG, "${document.data}")
                 }
             }
             .addOnFailureListener { exception ->
                 Log.w(TAG, "Error getting documents.", exception)
             }
+
+
+
+
         //nav inputs
         val profiel = findViewById<View>(R.id.nav_profiel) as TextView
         val velden = findViewById<View>(R.id.nav_veld) as TextView
