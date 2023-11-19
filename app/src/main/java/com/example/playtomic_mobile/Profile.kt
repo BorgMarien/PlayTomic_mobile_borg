84fc538c046556aa37cdf39a89b4c69a0726f1e6
BorgMarien <borgmarien@gmail.com>
2023-11-19 19:48:43 +0100

    finish nav v3

--- app/src/main/java/com/example/playtomic_mobile/Profile.kt
+++ app/src/main/java/com/example/playtomic_mobile/Profile.kt
@@ -1,11 +1,33 @@
 package com.example.playtomic_mobile
 
 import android.app.Activity
+import android.content.Intent
 import android.os.Bundle
+import android.view.View
+import android.widget.TextView
 
 class Profile : Activity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.profile_layout)
+
+        //nav inputs
+        val profiel = findViewById<View>(R.id.nav_profiel) as TextView
+        val velden = findViewById<View>(R.id.nav_veld) as TextView
+        val matchen = findViewById<View>(R.id.nav_match) as TextView
+        profiel.setOnClickListener{
+            val intent = Intent(applicationContext, Profile::class.java)
+            startActivity(intent)
+        }
+
+        velden.setOnClickListener{
+            val intent = Intent(applicationContext, Velden::class.java)
+            startActivity(intent)
+        }
+
+        matchen.setOnClickListener{
+            val intent = Intent(applicationContext, Matches::class.java)
+            startActivity(intent)
+        }
     }
 }
