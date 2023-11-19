1baaeacd5123e47b7f32dc777e630478d24ff5a6
BorgMarien <borgmarien@gmail.com>
2023-11-19 15:55:56 +0100

    add naviagtion test in velden ativity

--- app/src/main/java/com/example/playtomic_mobile/Velden.kt
+++ app/src/main/java/com/example/playtomic_mobile/Velden.kt
@@ -1,11 +1,22 @@
 package com.example.playtomic_mobile
 
 import android.app.Activity
+import android.content.Intent
 import android.os.Bundle
+import android.view.View
+import android.widget.ImageView
+
 
 class Velden: Activity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        //setContentView(R.layout.profile_layout)
+        setContentView(R.layout.velden_layout)
+
+        val button = findViewById<View>(R.id.navigationTest) as ImageView
+
+        button.setOnClickListener{
+            val intent = Intent(applicationContext, CreateMatch::class.java)
+            startActivity(intent)
+        }
     }
 }
