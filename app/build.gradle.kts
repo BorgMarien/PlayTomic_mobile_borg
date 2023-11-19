f010c2d713107c2b3448c49ffd1c1f5ba32929b5
BorgMarien <borgmarien@gmail.com>
2023-11-19 15:04:28 +0100

    change run settings

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -50,7 +50,7 @@ android {
 }
 
 dependencies {
-
+    implementation("androidx.compose.runtime:runtime:1.4.3")
     implementation("androidx.core:core-ktx:1.9.0")
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.9.0")
--- app/src/main/java/com/example/playtomic_mobile/MainActivity.kt
+++ app/src/main/java/com/example/playtomic_mobile/MainActivity.kt
@@ -1,17 +1,7 @@
 package com.example.playtomic_mobile
 
 import android.app.Activity
-import android.os.Bundle
-import androidx.activity.ComponentActivity
-import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import com.example.playtomic_mobile.ui.theme.Playtomic_mobileTheme
+
 
 class MainActivity : Activity() {
 
